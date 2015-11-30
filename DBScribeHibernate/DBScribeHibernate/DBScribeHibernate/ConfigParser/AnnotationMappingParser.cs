12c4e54df677419e9216c371c62d1add10c47b6f
dephien <bnie@email.wm.edu>
2015-11-30 17:43:12 -0500

    edits

--- DBScribeHibernate/DBScribeHibernate/DBScribeHibernate/ConfigParser/AnnotationMappingParser.cs
+++ DBScribeHibernate/DBScribeHibernate/DBScribeHibernate/ConfigParser/AnnotationMappingParser.cs
@@ -394,14 +394,5 @@ namespace DBScribeHibernate.DBScribeHibernate.ConfigParser
         {
             return tableNameToTableConstraints;
         }
-
-        private void _WaitCounter()
-        {
-            int x = 0;
-            for (int i = 0; i < 1000; i++)
-            {
-                x = i;
-            }
-        }
     }
 }
